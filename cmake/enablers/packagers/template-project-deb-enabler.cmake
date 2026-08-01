0cb72881401f5b98a01aca6ded6f7b737616882e
Yurii <68185017+yuriysydor1991@users.noreply.github.com>
2026-08-01 17:30:44 +0300

    Initialize appQt6

---
+++ cmake/enablers/packagers/template-project-deb-enabler.cmake
@@ -0,0 +1,31 @@
+cmake_minimum_required(VERSION 3.13)
+
+option(
+  ENABLE_DEB
+  "Set to ON to enable the deb package creation from the project build artifacts"
+  OFF
+)
+
+if(NOT ENABLE_DEB)
+    return()
+endif()
+
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_GENERATOR "DEB")
+set(CPACK_PACKAGE_NAME ${CMAKE_PROJECT_NAME})
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_PACKAGE_CONTACT ${PROJECT_MAINTAINER_EMAIL})
+set(CPACK_DEB_MAINTAINER "${PROJECT_MAINTAINER} ${PROJECT_MAINTAINER_EMAIL}")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${PROJECT_MAINTAINER} ${PROJECT_MAINTAINER_EMAIL}")
+set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_PACKAGE_DESCRIPTION ${CMAKE_PROJECT_DESCRIPTION})
+set(CPACK_DEB_PACKAGE_ARCHITECTURE "amd64")  # Or arm64
+set(CPACK_DEBIAN_PACKAGE_DEPENDS
+    "libqt6core6, libqt6gui6, libqt6widgets6, libqt6qml6, libqt6quick6, 
+    libqt6quickcontrols2-6, libqt6quicktemplates2-6, libqt6qmlworkerscript6, 
+    qml6-module-qtquick, qml6-module-qtqml-workerscript, 
+    qml6-module-qtquick-templates, qml6-module-qtquick-window"
+)
+
+include(CPack)
